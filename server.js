0fd64069cb7adf987814a8dbd3b6cf36e2568b31
aymafaza <garudamalang@gmail.com>
2021-03-28 19:53:35 +0700

    update port

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 server.listen(PORT, () => {
   console.log(`listening on *:${PORT}`);
